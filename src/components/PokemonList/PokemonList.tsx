31525756918388f08d73f77799d2b004d7f42346
2hanbyeol1 <2hanbyeol1@naver.com>
2024-07-05 13:35:05 +0900

    feat: 헤더 고정

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -6,9 +6,8 @@ interface PokemonListProps {
 }
 
 function PokemonList({ pokemons }: PokemonListProps) {
-  console.log(pokemons);
   return (
-    <li className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 justify-items-center gap-3">
+    <li className="mt-28 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 justify-items-center gap-3">
       {pokemons.map((pokemon) => (
         <PokemonCard key={pokemon.id} pokemon={pokemon} />
       ))}
